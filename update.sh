e52f52d05c8bb27e81beb544297ac91e37db7560
blac_razor <blacrazor@gmail.com>
2022-02-23 23:52:10 +0700

    LastFix

--- update.sh
+++ update.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cp /home/pi/macro_stand/UpdateStand /home/pi/Desktop/UpdateStand
+## Back up
+cp /home/pi/macro_stand/GetPhoto.desktop /home/pi/Desktop/GetPhoto.desktop
+cp /home/pi/macro_stand/UpdateStand.desktop /home/pi/Desktop/UpdateStand.desktop
 cp /home/pi/macro_stand/update.sh /home/pi/update.sh
+## Rm old version
 rm -rf /home/pi/macro_stand
+## Clone new version
 git clone https://github.com/BlacRazor/macro_stand.git
+## Update 
+cp /home/pi/macro_stand/GetPhoto.desktop /home/pi/Desktop/GetPhoto.desktop
+cp /home/pi/macro_stand/UpdateStand.desktop /home/pi/Desktop/UpdateStand.desktop
+cp /home/pi/macro_stand/update.sh /home/pi/update.sh
