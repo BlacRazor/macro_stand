b1aa9e06e2c10cbbf5e7175f72977c2debfa339e
blac_razor <blacrazor@gmail.com>
2022-02-23 23:42:42 +0700

    Fix updater

--- update.sh
+++ update.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-rm -f /home/pi/macro_stand
+cp /home/pi/macro_stand/UpdateStand /home/pi/Desktop/UpdateStand
+cp /home/pi/macro_stand/update.sh /home/pi/update.sh
+rm -rF /home/pi/macro_stand
 git clone https://github.com/BlacRazor/macro_stand.git
