d881575f3140e6768c6d9174ca1812dd3ce3827b
Blac <blacrazor@gmail.com>
2022-04-22 11:39:08 +0700

    change

--- test3.py
+++ test3.py
@@ -1,5 +1,6 @@
 import subprocess
 sample_name='test'
 i=1
-result=subprocess.run(["gphoto2 --capture-image-and-download --filename "+sample_name+"_"+str(i)+".jpg"],shell=True)
-print(result)
+result=subprocess.run(["gphoto2 --capture-image-and-download --filename "+sample_name+"_"+str(i)+".jpg"],shell=True,capture_output=True, text=True)
+print("stdout:", result.stdout)
+print("stderr:", result.stderr)
